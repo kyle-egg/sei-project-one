f69749624eda9e25090a7a65ea8e003275d500c3
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2021-07-16 13:38:20 +0100

    added-invaders

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 // * DOM Variables
 
 const grid = document.querySelector('.grid')
-// const start = document.querySelector('#start')
+const start = document.querySelector('#start')
 // const scoreDisplay = document.querySelector('#score-display')
 
 // * Game Variables
@@ -10,19 +10,31 @@ const width = 11
 const gridCellCount = width * width
 const cells = []
 
+
 // let timer
 // let score = 0
 let dogPosition = 115
-// let totalPikas = 0
+let hooverArray = [2, 3, 4, 5, 6, 7, 8]
+const hooverStartPosition = 2
+const hooverEndPositionEasy = 9
+
+console.log(hooverArray)
 
 // * Functions
 
-function addDog(dogposition) {
+function addDog(dogPosition) {
   cells[dogPosition].classList.add('dog')
 }
 
-function removeDog(position) {
+function removeDog() {
   cells[dogPosition].classList.remove('dog')
+}
+
+function addHooversEasy() {
+  for (let i = hooverStartPosition, j = 13; i <= hooverEndPositionEasy, j < 20; i++, j++) {
+  cells[i].classList.add('hoover')
+  cells[j].classList.add('hoover')
+}
 }
 
 // function generateRandomPikachuIndex() {
@@ -39,6 +51,7 @@ function createGrid() {
     cell.setAttribute('data-index', i)
     cells.push(cell)
     grid.appendChild(cell)
+    cell.textContent = i
   }
 }
 
@@ -73,21 +86,26 @@ document.addEventListener('keyup', moveDog)
 // }
 
 
-// function startGame() {
-//   timer = setInterval(() => {
-//     if (totalPikas > 9) {
-//       endGame()
-//       return
-//     }
-//     totalPikas++
-//     removePikachu(pikaPosition)
-//     pikaPosition = generateRandomPikachuIndex()
-//     addPikachu(pikaPosition)
-//   }, 1000)
-// }
+function startGame() {
+  console.log('meow')
+  timer = setInterval(() => {
+    // if (totalPikas > 9) {
+    //   endGame()
+    //   return
+    // }
+    totalhoovers++
+    // removePikachu(pikaPosition)
+    // pikaPosition = generateRandomPikachuIndex()
+    // addPikachu(pikaPosition)
+  }, 1000)
+}
 
+// startGame()
 createGrid()
 addDog(dogPosition)
+addHooversEasy()
 
 // * Events
-// start.addEventListener('click', startGame)
+start.addEventListener('click', startGame)
+
+
