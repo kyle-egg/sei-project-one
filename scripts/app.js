be54522885c4b483204e3a6966b0c57f7dd3c100
Kyle Eggelhoefer <keggelhoefer@gmail.com>
2021-07-20 12:42:57 +0100

    dog shoots

--- scripts/app.js
+++ scripts/app.js
@@ -10,7 +10,6 @@ const gridCellCount = width * width
 const cells = []
 
 let dogPosition = 115
-let dogShot = dogPosition - width
 const hooverStartPosition = 2
 const hooverEndPositionEasy = 9
 
@@ -30,23 +29,14 @@ function createGrid() {
 function endGame(){
   console.log('meow')
   clearInterval()
-  alert("You lose bitch")
-  }
+  alert('You lose bitch')
+}
 
 function startGame() {
-  // timer = setInterval(() => {
-  //   if (totalPikas > 9) {
-  //     endGame()
-  //     return
-  //   }
-  //   totalhoovers++
-  //   removePikachu(pikaPosition)
-  //   pikaPosition = generateRandomPikachuIndex()
-  //   addPikachu(pikaPosition)
-  // }, 1000)
+ 
   addHooversEasy()
   
-  cells[30].classList.add('end')
+  cells[111].classList.add('end')
   function addDog(dogPosition) {
     cells[dogPosition].classList.add('dog')
   }
@@ -56,87 +46,98 @@ function startGame() {
     cells[dogPosition].classList.remove('dog')
   }
 
-  // function addShot(dogShot) {
-  //   cells[dogShot].classList.add('dogshot')
-  // }
-
-  // function removeShot() {
-  //   cells[dogShot].classList.remove('dogshot')
-  // }
-
-
-
-function moveDog(event) {
-
-  removeDog(dogPosition) // * remove dog from the current position
-
-  const x = dogPosition % width
-
-  switch (event.keyCode) { // * calculate the next position and update it
-    case 39:
-      if (x < width - 1) dogPosition++
-      break
-    case 37:
-      if (x > 0) dogPosition--
-      break
-      
+  function addShot(dogShot) {
+    cells[dogShot].classList.add('dogshot')
   }
 
-  addDog(dogPosition) // * add dog to the new position
-}
-window.addEventListener('keyup', moveDog)
+  function removeShot() {
+    cells[dogShot].classList.remove('dogshot')
+  }
 
-// function shootDog(event) {  
-//   switch (event.keyCode) {
-//     case 16:
-//     addShot(dogShot)++ // * add shot
-//     break
-// }
-//   addShot(dogShot)
-// }
 
-// window.addEventListener('keypress', shootDog)
+
+  function moveDog(event) {
+
+    removeDog(dogPosition) // * remove dog from the current position
+
+    const x = dogPosition % width
+
+    switch (event.keyCode) { // * calculate the next position and update it
+      case 39:
+        if (x < width - 1) dogPosition++
+        break
+      case 37:
+        if (x > 0) dogPosition--
+        break
+    }
+    // console.log(dogPosition)
+    addDog(dogPosition) // * add dog to the new position
+  }
+  window.addEventListener('keyup', moveDog)
+
+  function shootDog(e) {  
+    let dogShot = dogPosition
+    console.log(dogPosition)
+    console.log(dogShot)
+    if (e.keyCode === 38) {
+      setInterval(() => {
+        cells[dogShot].classList.remove('dogshot')
+        dogShot -= width
+        cells[dogShot].classList.add('dogshot')
+      }, 500)
+    }
+
+    //   switch (e.key) {
+    //     case 32:
+    //     addShot(dogShot - width) // * add shot
+    //     break
+
+    // }
+    //   addShot(dogShot)
+  }
+
+  window.addEventListener('keydown', shootDog)
 
   function addHooversEasy() {
     for (let i = hooverStartPosition, j = 13; i <= hooverEndPositionEasy, j < 20; i++, j++) {
-    cells[i].classList.add('hoover')
-    cells[j].classList.add('hoover')
+      cells[i].classList.add('hoover')
+      cells[j].classList.add('hoover')
 
-    setInterval(() => {
-    cells[i++].classList.add('hoover')
-    cells[j++].classList.add('hoover')
+      setInterval(() => {
+        cells[i++].classList.add('hoover')
+        cells[j++].classList.add('hoover')
       
-    if (cells[j].classList.contains('end', 'hoover')) {
-    endGame()
-    clearInterval()
-    }
+        if (cells[j].classList.contains('end', 'hoover')) {
+          endGame()
+          clearInterval()
+        }
 
-    // cells[j + 20].classList.add('hoover')
-    // if (cells[j].classList.contains('hoover')) {
-    // for (let i = 50; i <= 55; i++) {
-    //     cells[i].classList.add('hoover')
-    // }}
-    // console.log(cells[i++])
-    // console.log(cells[j++])
-    // if (cells[i++] < width - 1) cells[i + 50]
+        // cells[j + 20].classList.add('hoover')
+        // if (cells[j].classList.contains('hoover')) {
+        // for (let i = 50; i <= 55; i++) {
+        //     cells[i].classList.add('hoover')
+        // }}
+        // console.log(cells[i++])
+        // console.log(cells[j++])
+        // if (cells[i++] < width - 1) cells[i + 50]
       // brea
-    // case 37:
-    //   if (x > 0) dogPosition--
-    //   break
-    // cells[i--].classList.remove('hoover')
-  }, 1000)
-  }
+        // case 37:
+        //   if (x > 0) dogPosition--
+        //   break
+        // cells[i--].classList.remove('hoover')
+      }, 1000)
+    }
   }
 
   function removeHooversEasy() {
-    for (let i = 0, j = 11; i <= 1, j<= 12; i++, j++) {
-    setInterval(() => {
-    cells[i++].classList.remove('hoover')
-    cells[j++].classList.remove('hoover')
-    }, 1000)
+    for (let i = 0, j = 11; i <= 1, j <= 12; i++, j++) {
+      setInterval(() => {
+        cells[i++].classList.remove('hoover')
+        cells[j++].classList.remove('hoover')
+      }, 1000)
+    }
   }
-}
-removeHooversEasy()
+  removeHooversEasy()
 
 }
 
